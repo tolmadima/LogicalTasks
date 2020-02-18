7d55e06f2ab0fd6d7489d6a3485caec75ba30f35
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-19 01:31:55 +0300

    Problems with arrays

--- EvenNumbers/src/EvenNumberOfDIgits.java
+++ EvenNumbers/src/EvenNumberOfDIgits.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class EvenNumberOfDIgits {
     public static int findNumbers(int[] nums) {
-        
+        for (int i=0; i>number; i = i++;){
+            System.out.println("Введите число номер " + i);
+            Scanner scan = new Scanner(System.in);
+            int number = scan.nextInt();
+            findNumbers(i) = number;
+            System.out.println(+ findNumbers(i));
+        }
         return findNumbers();
+    }
+    public static void main(String Args[]){
+        System.out.println("Введите количество чисел в ряду");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        findNumbers(number);
     }
 }
