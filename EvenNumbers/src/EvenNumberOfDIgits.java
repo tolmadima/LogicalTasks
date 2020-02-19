f09fb7e5a762d325655574c6cb0d6d23e17e3146
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-19 15:11:54 +0300

    Reassembling whole code

--- EvenNumbers/src/EvenNumberOfDIgits.java
+++ EvenNumbers/src/EvenNumberOfDIgits.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
-public class EvenNumberOfDIgits {
-    public static int findNumbers(int[] nums) {
-        for (int i=0; i>number; i = i++;){
+class EvenNumberOfDIgits {
+    public static int[] NewArray;
+    public static int j;
+    public static int findNumbers(int nums) {
+        j = nums;
+        for (int i=1; i<nums; i++ ){
+            NewArray = new int[nums];
             System.out.println("Введите число номер " + i);
-            Scanner scan = new Scanner(System.in);
-            int number = scan.nextInt();
-            findNumbers(i) = number;
-            System.out.println(+ findNumbers(i));
+            Scanner scanf = new Scanner(System.in);
+            int number = scanf.nextInt();
+            NewArray[i] = number;
+            System.out.println(+ NewArray[i]);
         }
-        return findNumbers();
+        return 0;
     }
+
+    public static int EvenNumber(int Number){
+        for (int i=1; i<j; i++) {
+            int a = NewArray[i];
+
+        }
+    }
+
     public static void main(String Args[]){
-        System.out.println("Введите количество чисел в ряду");
+        System.out.println("Введите количество значений массива");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         findNumbers(number);
+
     }
 }
