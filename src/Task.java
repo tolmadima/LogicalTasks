88bd6e345bac9f58a6a50a2668d21adefbdf2ce4
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-19 18:50:21 +0300

    New file

--- src/Task.java
+++ src/Task.java
@@ -3,6 +3,20 @@ public class Task {
 
 
 
+    public static int substractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+        while (n != 0) {
+            product = n % 10 * product;
+            sum = n % 10 + sum;
+            n = n / 10;
+        }
+        int substract = product - sum;
+        return substract;
+    }
+
+
+
     public static int countnumbers(int[] mass) {
         int count = 0;
 
@@ -40,6 +54,12 @@ public class Task {
 
 
 
+    public static String defanIPaddr(String address) {
+        String defanIPaddr = address.replace(".", "[.]");
+        return defanIPaddr;
+    }
+
+
     public static void main(String Args[]) {
         int[] massive = {1, 2, 3, 4, 5, 6};
         for (int i = 1; i < massive.length; ++i) {
@@ -51,10 +71,21 @@ public class Task {
         }
 
         System.out.print("Введите любое целое число : ");
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        System.out.println("Количество шагов равно = " + numberOfSteps(number));
+        Scanner scane = new Scanner(System.in);
+        int numbers = scane.nextInt();
+        System.out.println("Количество шагов равно = " + numberOfSteps(numbers));
 
+
+        System.out.println("Введите число :")
+        Scanner scan = new Scanner (System.in);
+        int number = scan.nextInt();
+        System.out.println("Substract = " + substractProductAndSum(number));
+
+
+
+        Scanner scans = new Scanner(System.in);
+        String IP = scans.nextLine();
+        System.out.println(defanIPaddr(IP));
 
 
     }
